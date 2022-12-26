1f1870d28be3c7f64055f0a75ee5bfe910a6ce0f
aminogira <aminogira@gmail.com>
2022-12-26 13:25:31 +0530

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String args[]){
+        System.out.println("this is Amila from Giragama");
+        System.out.println("My full name is Amila Kanishka Bandara Giragama");
+        System.out.println("I am a Senior Software Engineer");
+    }
+}
