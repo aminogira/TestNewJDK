ec3c9caa44eecebc5730612119764816806a8be0
aminogira <aminogira@gmail.com>
2022-12-26 14:29:53 +0530

    My Info Update2

--- Main.java
+++ Main.java
@@ -5,5 +5,7 @@ public class Main {
         System.out.println("My full name is Amila Kanishka Bandara Giragama");
         System.out.println("I am a Senior Software Engineer");
         System.out.println("I went to DRCK");
+        System.out.println("My 1st job was Instructure in NYSC Polgolla");
+
     }
 }
