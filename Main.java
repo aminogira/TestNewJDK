dc76012f469c9fe99e9e93f5259be166b4b4dd51
aminogira <aminogira@gmail.com>
2022-12-26 16:26:05 +0530

    Method Calls

--- Main.java
+++ Main.java
@@ -1,11 +1,20 @@
 public class Main {
     public static void main(String args[]){
+        displayMyInfoPersonal();
+        displayEducation();
+        displayExperience();
+    }
+    private static void displayMyInfoPersonal(){
         System.out.println("I am Amila Giragama");
         System.out.println("I live in Pilimathalawa");
         System.out.println("My full name is Amila Kanishka Bandara Giragama");
         System.out.println("I am a Senior Software Engineer");
-        System.out.println("I went to DRCK");
-        System.out.println("My 1st job was Instructure in NYSC Polgolla");
-
     }
+    private static void displayEducation() {
+        System.out.println("I went to DRCK");
+    }
+    private static void displayExperience() {
+        System.out.println("My 1st job was Instructure in NYSC Polgolla");
+    }
+
 }
--- out/production/TestNewJDK/README.md
+++ out/production/TestNewJDK/README.md
@@ -1,1 +1,20 @@
 # TestNewJDK
+author Amila develop a git understanding project
+
+## Installation
+Use the package manager [java](https://www.java.com/en/download/manual.jsp) to install foobar.
+
+```bash
+java -jar TestNewJDK.jar
+```
+
+```java
+
+
+# returns 'words'
+private static String maingetWords();
+
+# returns 'geese'
+private static String getGeese();
+
+```
