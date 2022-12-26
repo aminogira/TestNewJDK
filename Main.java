c8a29bcdc3dea75d1021d6f1775198db1a9ddcda
aminogira <aminogira@gmail.com>
2022-12-26 14:02:17 +0530

    My Info Update

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String args[]){
-        System.out.println("this is Amila from Giragama");
+        System.out.println("I am Amila Giragama");
+        System.out.println("I live in Pilimathalawa");
         System.out.println("My full name is Amila Kanishka Bandara Giragama");
         System.out.println("I am a Senior Software Engineer");
+        System.out.println("I went to DRCK");
     }
 }
